674194968727075e8552b97da64d884e951e5f3e
Manisha Mali <82174253+M74-dot@users.noreply.github.com>
2024-07-28 20:08:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded',function(){
+	document.getElementById('calculate').addEventListener('click',function(){
+		const num1 = document.getElementById('num1').value;
+		const num2 = document.getElementById('num2').value;
+		const operator = document.getElementById('ops').value;
+		let result;
+
+		if(!isNaN(num1) && !isNaN(num2)){
+			switch(operator){
+				case 'plus':
+					result = num1 + num2;
+					break;
+				case 'minus':
+					result = num1 - num2;
+					break;
+				case 'mult':
+					result = num1 * num2;
+					break;
+				case 'divide':
+					if(num2!==0){
+						result = num1 / num2;
+					}else{
+						result = "cannot divide by zero";
+					}
+					break;
+				default:
+					result = "Invalid Operator";
+			}
+		}else{
+			result="Invalid Input";
+		}
+		document.getElementById('result').textContent = result;
+	});
+
+	document.getElemetnById('change_text').addEventListener('click',function(){
+		const divs = document.getElementsByClassName('division');
+		const newVal = ['Bye','Good Bye','To','Your','Class'];
+		for(let i=0; i<divs.length; i++){
+			divs[i].textContent = newVal[i];
+		}
+	});
+});
