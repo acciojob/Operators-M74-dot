00d9eb072389c34daa6789cf87a9fe4c3dddea21
Manisha Mali <82174253+M74-dot@users.noreply.github.com>
2024-07-28 20:30:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 //your JS code here. If required.
 document.addEventListener('DOMContentLoaded',function(){
 	document.getElementById('calculate').addEventListener('click',function(){
-		const n1 = document.getElementById('num1').value;
-        const n2 = document.getElementById('num2').value;
-		const num1 = Number(n1);
-		const num2 = Number(n2);
+		const num1 = parseFloat(document.getElementById('num1').value);
+        const num2 = parseFloat(document.getElementById('num2').value);
 		
-		const operator = document.getElementById('ops').value;
+		const operator = document.getElementById('operator').value;
 		let result;
 
 		if(!isNaN(num1) && !isNaN(num2)){
