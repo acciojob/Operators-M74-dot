57bd32ad17309dba7838dbdbf7b0d1d18ae9ce13
Manisha Mali <82174253+M74-dot@users.noreply.github.com>
2024-07-28 20:09:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //your JS code here. If required.
 document.addEventListener('DOMContentLoaded',function(){
 	document.getElementById('calculate').addEventListener('click',function(){
-		const num1 = document.getElementById('num1').value;
-		const num2 = document.getElementById('num2').value;
+		const num1 = parseFloat(document.getElementById('num1').value);
+		const num2 = parseFloat(document.getElementById('num2').value);
 		const operator = document.getElementById('ops').value;
 		let result;
 
@@ -33,7 +33,7 @@ document.addEventListener('DOMContentLoaded',function(){
 		document.getElementById('result').textContent = result;
 	});
 
-	document.getElemetnById('change_text').addEventListener('click',function(){
+	document.getElementById('change_text').addEventListener('click',function(){
 		const divs = document.getElementsByClassName('division');
 		const newVal = ['Bye','Good Bye','To','Your','Class'];
 		for(let i=0; i<divs.length; i++){
